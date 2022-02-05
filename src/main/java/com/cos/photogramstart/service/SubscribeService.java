de7e5014f59d6d1dd1f60fdfbd37240df6c0b419
hanks010 <hanks010@naver.com>
2022-02-05 16:32:25 +0900

    구독기능 예외처리

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
+import com.cos.photogramstart.handler.ex.CustomApiException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,11 +13,16 @@ import lombok.RequiredArgsConstructor;
 public class SubscribeService {
 
 	private final SubscribeRepository subscribeRepository;
-	
+
 	@Transactional
 	public void subscribe(int fromUserId, int toUserId) {
+		try {
 		subscribeRepository.mSubscribe(fromUserId, toUserId);
-	}
+		}catch(Exception e){
+			throw new CustomApiException("이미 구독을 하였습니다");
+		}
+		}
+
 	@Transactional
 	public void unSubscribe(int fromUserId, int toUserId) {
 		subscribeRepository.mUnSubscribe(fromUserId, toUserId);
